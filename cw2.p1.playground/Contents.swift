2238430774327cce8eacb6b8aea1b1d227923275
faisal-510 <66562768+faisal-510@users.noreply.github.com>
2020-06-17 21:06:43 +0300

    done

---
+++ cw2.p1.playground/Contents.swift
@@ -0,0 +1,13 @@
+let family =  ["Abdullatif", "Faisal", "Saleh"]
+
+var evens = [2, 4, 6, 8, 10, 12,]
+
+var grades = [97.2, 93.7, 91.0]
+
+var trueFalse = [true, false, true, true]
+
+print(family[0], family[2])
+print(evens[0], evens[5])
+print(grades[0], grades[2])
+print(trueFalse[0], trueFalse[3])
+
